4c188c63de1b4a7a2f643d0aaf05b723b8ad4604
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-01-10 14:00:25 +0300

    Initial Commit

---
+++ GameOfThrones/GameOfThronesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfThronesApp.swift
+//  GameOfThrones
+//
+//  Created by Ibragim Akaev on 10/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct GameOfThronesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
